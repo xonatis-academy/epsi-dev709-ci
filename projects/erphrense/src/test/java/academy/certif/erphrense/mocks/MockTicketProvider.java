1f489a92c15700fb965f714e54e3d3a63757af6c
hmicn <michael@xonatis.com>
2022-11-05 11:37:58 +0100

    Adding courses

---
+++ projects/erphrense/src/test/java/academy/certif/erphrense/mocks/MockTicketProvider.java
@@ -0,0 +1,13 @@
+package academy.certif.erphrense.mocks;
+
+import academy.certif.erphrense.domain.Cancelable;
+import academy.certif.erphrense.provider.CancelableProvider;
+
+public class MockTicketProvider extends CancelableProvider {
+    
+    @Override
+    public void persist(Cancelable cancelable) {
+        
+    }
+
+}
--- projects/erphrense/src/test/java/academy/certif/erphrense/service/BookingServiceTest.java
+++ projects/erphrense/src/test/java/academy/certif/erphrense/service/BookingServiceTest.java
@@ -7,8 +7,8 @@ import org.junit.Test;
 
 import academy.certif.erphrense.domain.Cancelable;
 import academy.certif.erphrense.domain.Ticket;
+import academy.certif.erphrense.mocks.MockTicketProvider;
 import academy.certif.erphrense.provider.CancelableProvider;
-import academy.certif.erphrense.provider.TicketProvider;
 
 public class BookingServiceTest 
 {
@@ -17,7 +17,7 @@ public class BookingServiceTest
 
     @BeforeClass
     public static void init() {
-        BookingServiceTest.ticketProvider = new TicketProvider();
+        BookingServiceTest.ticketProvider = new MockTicketProvider();
         BookingServiceTest.bookingService = new BookingService(ticketProvider);
     }
 
@@ -27,7 +27,6 @@ public class BookingServiceTest
         Ticket ticket = new Ticket("Annie Versaire", "01/01/2021 16:30", "Paris", "New-York", 0);
         assertTrue(ticket.getReference() == 0);
         Cancelable result = BookingServiceTest.bookingService.book(null, ticket);
-        ticket = (Ticket) result;
-        assertTrue(ticket.getReference() != 0);
+        assertTrue(result.getReference() != 0);
     }
 }
